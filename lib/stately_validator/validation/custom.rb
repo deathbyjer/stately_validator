5b14b67a756449f46cb3bdc8e5385e966ce029ec
jer <deathbyjer@gmail.com>
2017-07-12 18:33:55 -0400

    allow validation by a method. also add regexp expectations

--- lib/stately_validator/validation/custom.rb
+++ lib/stately_validator/validation/custom.rb
@@ -1,21 +1,49 @@
 module StatelyValidator
   class Validation
     
+    # This just will add an expectation concept into a Validation
+    module Expectations
+      def compare_with_expectation(expects, value)
+        return value.to_s =~ expects if expects.is_a?(Regexp)
+        value.eql?(expects)
+      end
+    end
+    
+    # The CUSTOM type of Validator allows us to do custom methods, defined on the validator or elsewhere
     class Custom < Base
       key :custom
+      
+      include Expectations
       
       def self.validate(values, name = [], options = {})
         # What is the expected output of this function? If it doesn't match, then throw the error
         expects = options.key?(:expects) ? options[:expects] : true
         
         # If sending in a class and method
-        return "error" if options[:class].is_a?(Module) && options[:method].is_a?(Symbol) && options[:class].respond_to?(options[:method]) && expects != options[:class].send(options[:method], *Utilities.to_array(values))
+        return "error" if options[:class].is_a?(Module) && options[:method].is_a?(Symbol) && options[:class].respond_to?(options[:method]) && !compare_with_expectation(expects, options[:class].send(options[:method], *Utilities.to_array(values)))
         
         # If sending a method that can be run on the attached validator
-        return "error" if options[:validator].is_a?(Validator::Base) && options[:method].is_a?(Symbol) && options[:validator].respond_to?(options[:method]) && expects != options[:validator].send(options[:method], *Utilities.to_array(values))
+        return "error" if options[:validator].is_a?(Validator::Base) && options[:method].is_a?(Symbol) && options[:validator].respond_to?(options[:method]) && !compare_with_expectation(expects, options[:validator].send(options[:method], *Utilities.to_array(values)))
         
         # And finally, if sending a Proc
-        return "error" if options[:proc].is_a?(Proc) && expects != options[:proc].call(*Utilities.to_array(values))
+        return "error" if options[:proc].is_a?(Proc) && !compare_with_expectation(expects, options[:proc].call(*Utilities.to_array(values)))
+        
+        true
+      end
+    end
+    
+    # The METHOD typeo f validator allows us to call methods on returned elements. These methods can then be evaluated for their result
+    class Method < Base
+      key :method
+      
+      include Expectations
+      
+      def self.validate(values, name = [], options = {})
+        return nil unless options[:method].is_a?(Symbol)
+        
+        method = options[:method]
+        expects = options.key?(:expects) ? options[:expects] : true
+        return "error" unless compare_with_expectation(expects, Utilities.to_array(values).all?{|item| item.respond_to?(method) && item.send(method, *Utilities.to_array(options[:args]))})
         
         true
       end
