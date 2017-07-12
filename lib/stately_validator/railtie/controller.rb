87c2230f844c49f0225b67c0da79abc8ec392b9f
jer <deathbyjer@gmail.com>
2017-07-12 13:30:05 -0400

    needed to use the instance, not class

--- lib/stately_validator/railtie/controller.rb
+++ lib/stately_validator/railtie/controller.rb
@@ -6,8 +6,8 @@ module StatelyValidator
       
       def validate_with(name, options = {})
         # Find the validator
-        validator = load_validator name
-        return nil unless validator
+        validator_class = load_validator name
+        return nil unless validator_class
         
         validator.set_action_controller self
         validator.validate(params) unless options[:dont_validate]
@@ -21,7 +21,7 @@ module StatelyValidator
         validator = nil
         validator = name if name.is_a?(StatelyValidator)
         validator = Validator.validator_for(name) if name.is_a?(Symbol)
-        validator
+        validator ? validator.new : nil
       end
     end
   end
