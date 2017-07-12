91cfb046d81974fd2763ef0fb436dc4bf3943c27
jer <deathbyjer@gmail.com>
2017-07-12 11:25:25 -0400

    update base to allow state

--- lib/stately_validator/validator/base.rb
+++ lib/stately_validator/validator/base.rb
@@ -21,6 +21,19 @@ module StatelyValidator
         @validations << { fields: fields, validation: validation, options: options }
       end
       
+      def self.validations
+        @validations || []
+      end
+      
+      def self.notes
+        @notes || []
+      end
+      
+      def self.note(field)
+        @notes = [] unless @notes.is_a?(Array)
+        @notes << field
+      end
+      
       def params=(params)
         @ran = false
         if params.is_a?(Hash)
@@ -34,6 +47,14 @@ module StatelyValidator
         return unless name.is_a?(Symbol)
         @params = {} unless @params.is_a?(Hash)
         @params[name] = value
+      end
+      
+      # This is how we can set some internal variables, which are useful for 
+      # validating. 
+      # A note: An internal will be overloaded 
+      def set_state(name, value)
+        @states = {} unless @states.is_a?(Hash)
+        @states[name.to_sym] = value
       end
       
       def params
@@ -52,7 +73,9 @@ module StatelyValidator
         return false unless @params.is_a?(Hash)
  
         @errors = {}
-      
+        @internal = {}
+        @notes = {}; self.class.notes.each {|n| @notes[n] = @params[n] if @params[n]}
+        
         self.class.validations.each do |details|
           # Are we skipping this because some of the items have failed their validations?
           opts = details[:options]
@@ -62,13 +85,16 @@ module StatelyValidator
           vals = Utilities.to_array(details[:fields]).map{|f| @params[f]}
           vals = vals.first if vals.count == 1
           
+          # Now we are going to skip based on internal errors, external errors and state
+          next if skip_validation?(opts)
+          
           # Attempt the validation
           err = Validation.validate(vals, details[:fields], details[:validation], self, opts)
           # Pass if the error is nil or is TrueClass
           next if err.nil? || err == true
           
           err = opts[:error] if opts[:error]
-          @errors[opts[:as] || Utilities.to_array(details[:fields]).first] = err
+          (opts[:internal] ? @internal : @errors)[opts[:as] || Utilities.to_array(details[:fields]).first] = err
         end
         
         @ran = true
@@ -88,6 +114,47 @@ module StatelyValidator
       
       def self.validations
         @validations || []
+      end
+      
+      def skip_validation?(opts)
+        # We are going to evaluate the skip_if condition
+        return evaluate_skip_array(opts[:skip_if], :and) if opts[:skip_if]
+        return !evaluate_skip_array(opts[:skip_unless], :and) if opts[:skip_unless]
+        false
+      end
+      
+      # We are going to evaluate the skip array and see if all the values 
+      def evaluate_skip_array(conditions, operator = :and)
+        conditions = [ conditions ] unless conditions.is_a?(Array)
+        operator == :and ? conditions.all?{|c| evaluate_skip_condition(c, operator)} : conditions.any?{|c| evaluate_skip_condition(c, option)}
+      end
+      
+      def evaluate_skip_condition(condition, operator = :and)
+        # If the condition is a string, convert it to a symbol
+        condition = condition.to_sym if condition.is_a?(String)
+        
+        # If the condition is just the existence of an error or an internal error, then just check for that
+        return @states[condition] || @notes[condition] || @internal[condition] || @errors[:condition] if condition.is_a?(Symbol)
+        
+        # If the condition is an array, then we need to evaluate it according to the opposite operator of this array
+        return evaluate_skip_array(condition, operator == :and ? :or : :and) if condition.is_a?(Array)
+        
+        # The only thing left to evaluate is a hash. So if it's not a hash, return that this is fine 
+        return true unless condition.is_a?(Hash)
+        
+        # If the condition is a hash, then it depends on the size of the hash.
+        # IF the size of the has is 0, then it evaluates to true
+        return true if condition.empty?
+        
+        # If the condition has more than one item, turn it into an array of one-item conditions
+        return evaluate_skip_array(condition.keys.map{|k| {k => condition[k]}}, operator == :and ? :or : :and) if condition.count > 1
+        
+        # Now we need to evaluate the condition hash
+        k,v = condition.first
+        [@errors, @internal, @states, @notes].each { |check| return true if check[k].eql?(v) }
+        
+        # Otherwise, just return false
+        false
       end
     end
     
