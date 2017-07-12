2ea0bc7b472b06f3f0ecd89884f1fbffaa86f7a0
jer <deathbyjer@gmail.com>
2017-07-12 18:35:09 -0400

    allow an array of regexp

--- lib/stately_validator/validation/matches.rb
+++ lib/stately_validator/validation/matches.rb
@@ -6,7 +6,12 @@ module StatelyValidator
       
       def self.validate(values, names = [], options = {})
         return "incorrect" if options[:in] && options[:in].is_a?(Array) && !(Utilities.to_array(values) - options[:in]).empty?
-        return "incorrect" if options[:regex] && options[:regex].is_a?(Regexp) && !Utilities.to_array(values).all?{|v| v.to_s =~ options[:regex]}
+        
+        if options[:regex]
+          Utilities.to_array(options[:regex]).each do |regex|
+            return "incorrect" unless regex.is_a?(Regexp) && Utilities.to_array(values).all?{|v| v.to_s =~ regex}}
+          end
+        end
         true
       end
     end
