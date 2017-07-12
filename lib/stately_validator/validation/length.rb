e7a2dc21c5afbddf433fa2178e6d87e432caae58
jer <deathbyjer@gmail.com>
2017-07-12 18:34:49 -0400

    allow exact length matches

--- lib/stately_validator/validation/length.rb
+++ lib/stately_validator/validation/length.rb
@@ -5,6 +5,7 @@ module StatelyValidator
       key :length
       
       def self.validate(value, names = [], options = {})
+        return "incorrect" if options[:eq] && value.length != options[:eq]
         return "too_short" if options[:min] && value.length < options[:min]
         return "too_long" if options[:max] && value.length > options[:max]
         true
