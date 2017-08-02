a8a79f01f48053e018de856588de7536d8338bc8
jer <deathbyjer@gmail.com>
2017-08-02 13:13:05 -0400

    added key to matches

--- lib/stately_validator/validation/matches.rb
+++ lib/stately_validator/validation/matches.rb
@@ -6,6 +6,7 @@ module StatelyValidator
       
       def self.validate(values, names = [], options = {})
         return "incorrect" if options[:in] && options[:in].is_a?(Array) && !(Utilities.to_array(values) - options[:in]).empty?
+        return "incorrect" if options[:key] && options[:key].is_a?(Hash) && Utilities.to_array(values).any?{|k| not options[:key][k]}
         
         if options[:regex]
           Utilities.to_array(options[:regex]).each do |regex|
