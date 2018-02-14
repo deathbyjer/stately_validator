fe0c9a72c398dacad57a759e1551679113c434b5
jer <deathbyjer@gmail.com>
2018-02-14 10:55:15 -0500

    custom methods allow for errors

--- lib/stately_validator/validation/custom.rb
+++ lib/stately_validator/validation/custom.rb
@@ -19,14 +19,18 @@ module StatelyValidator
         # What is the expected output of this function? If it doesn't match, then throw the error
         expects = options.key?(:expects) ? options[:expects] : true
         
-        # If sending in a class and method
-        return "error" if options[:class].is_a?(Module) && options[:method].is_a?(Symbol) && options[:class].respond_to?(options[:method]) && !compare_with_expectation(expects, options[:class].send(options[:method], *Utilities.to_array(values)))
-        
-        # If sending a method that can be run on the attached validator
-        return "error" if options[:validator].is_a?(Validator::Base) && options[:method].is_a?(Symbol) && options[:validator].respond_to?(options[:method]) && !compare_with_expectation(expects, options[:validator].send(options[:method], *Utilities.to_array(values)))
-        
-        # And finally, if sending a Proc
-        return "error" if options[:proc].is_a?(Proc) && !compare_with_expectation(expects, options[:proc].call(*Utilities.to_array(values)))
+        begin
+          # If sending in a class and method
+          return "error" if options[:class].is_a?(Module) && options[:method].is_a?(Symbol) && options[:class].respond_to?(options[:method]) && !compare_with_expectation(expects, options[:class].send(options[:method], *Utilities.to_array(values)))
+          
+          # If sending a method that can be run on the attached validator
+          return "error" if options[:validator].is_a?(Validator::Base) && options[:method].is_a?(Symbol) && options[:validator].respond_to?(options[:method]) && !compare_with_expectation(expects, options[:validator].send(options[:method], *Utilities.to_array(values)))
+          
+          # And finally, if sending a Proc
+          return "error" if options[:proc].is_a?(Proc) && !compare_with_expectation(expects, options[:proc].call(*Utilities.to_array(values)))
+        rescue
+          return "error" unless expects == :error
+        end
         
         true
       end
