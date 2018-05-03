bf4c539a7c67b512f9a29b831c2ae680625cdd70
jer <deathbyjer@gmail.com>
2018-05-03 12:48:51 -0400

    ensure all the eaches

--- lib/stately_validator/railtie/controller.rb
+++ lib/stately_validator/railtie/controller.rb
@@ -30,6 +30,7 @@ module StatelyValidator
         validator.set_action_controller self
         (@validator_states || {}).each {|n,v| validator.set_state n, v}
         
+        Rails.logger.info params.send(params.respond_to?(:to_unsafe_h) ? :to_unsafe_h : :to_h).inspect
         p = {}; params.send(params.respond_to?(:to_unsafe_h) ? :to_unsafe_h : :to_h).each {|k,v| p[k.to_s.to_sym] = v}
         
         validator.params = p
